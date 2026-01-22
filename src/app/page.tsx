50926068929c58292a0c31cd992478987abd42ec
Ankit Ojha <ankitojha1409@gmail.com>
2026-01-22 14:56:19 +0530

    imported convex for database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import { SignedIn, SignedOut, SignOutButton, SignUpButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut, SignOutButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex justify-center items-center'>
+    <div className='flex justify-center items-center gap-4 h-screen'>
       <SignedOut>
         <SignUpButton>
           <button className='bg-blue-500 rounded-full p-2'>Sign In</button>
         </SignUpButton>
       </SignedOut>
+      <UserButton/>
       <SignedIn>
         <SignOutButton>
           <button className='bg-red-500 rounded-full p-2'>Sign Out</button>
