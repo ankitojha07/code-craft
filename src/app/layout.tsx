a615fd517d491902aa3ba5ef395e3af46d20f777
Ankit Ojha <ankitojha1409@gmail.com>
2026-01-22 15:07:48 +0530

    Added convex auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,12 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>{children}</ClerkProvider>
+        <ConvexClientProvider>
+        {children}
+        </ConvexClientProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
