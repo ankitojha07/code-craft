c7a7a1eaa7cebcf4b9e980d9080ed754617de1ae
Ankit Ojha <ankitojha1409@gmail.com>
2026-01-22 14:43:10 +0530

    sign-in and sigh out with clrek

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex justify-center items-center'>
       <SignedOut>
         <SignUpButton>
-          <button className='bg-blue-500 rounded-full p-2'>Sign Up</button>
+          <button className='bg-blue-500 rounded-full p-2'>Sign In</button>
         </SignUpButton>
       </SignedOut>
       <SignedIn>
         <SignOutButton>
-          <button className='bg-blue-500 rounded-full p-2'>Sign In</button>
+          <button className='bg-red-500 rounded-full p-2'>Sign Out</button>
         </SignOutButton>
       </SignedIn>
     </div>
